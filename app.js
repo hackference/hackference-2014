f66fd108a4d74c18a4efe2f1a2ca8f6cc16ac66c
Mike Elsmore <mikee@winningmoves.com>
2014-02-02 19:31:23 +0000

    Use local images with fallback to Lanyrd

--- app.js
+++ app.js
@@ -9,7 +9,9 @@
 /* Initial Requires for App */
 var express = require('express'),
     stylus = require('stylus'),
-    nib = require('nib');
+    nib = require('nib'),
+    passport = require('passport'),
+    fs = require('fs');
 
 /* Set Up App */
 process.env.TZ = 'Europe/London';
@@ -42,33 +44,45 @@ app.locals({
 app.get('/', function (req, res) {
 
     // Process speaker list - Ignore myself
-    var Lanyrd = require('lanyrd');
-    Lanyrd.speakers('hackference','2014',function(error, response, returnObj ){
-        var speakerObject = {};
-        var counter = 0;
-        for(var i = 0; i < returnObj.length; i++){
-            if(returnObj[i].title !== 'Michael Elsmore'){
-                var twitterUrl = '';
-                for(var profileLinksIterator = 0; profileLinksIterator < returnObj[i].profile_links.length; profileLinksIterator++){
-                    if(returnObj[i].profile_links[profileLinksIterator].icon_class === 'twitter'){
-                        twitterUrl = returnObj[i].profile_links[profileLinksIterator].url;
-                        break;
-                    }
-                }
-                var speaker = {
-                    'name':returnObj[i].title,
-                    'image':returnObj[i].image,
-                    'twitter':twitterUrl,
-                    'break':(((counter+1)%4==0)?'clearfix':'')
-                };
-                speakerObject[counter] = speaker;
-                counter++;
-            }
-        }
-        res.render('index',{
-            'title':app.locals.title,
-            'speakerObject' : speakerObject
+    var http = require('http');
+    var url = 'http://lanyrd.com/2014/hackference/speakers/46e95581d4f23c37.v1.json';
+
+    http.get(url, function(response) {
+        var body = '';
+
+        response.on('data', function(chunk) {
+            body += chunk;
         });
+
+        response.on('end', function() {
+            var lanyrdResponse = JSON.parse(body).speakers;
+            var counter = 0;
+            var speakerObject = {};
+            var speakerImagePath = './public/images/speakers/'
+            for(var i = 0; i < lanyrdResponse.length; i++){
+                if(lanyrdResponse[i].name !== 'Michael Elsmore'){
+
+                    var twitter = lanyrdResponse[i].twitter.substring(1);
+
+                    var speaker_image = (fs.existsSync(speakerImagePath+twitter+'.jpg'))?(speakerImagePath+twitter+'.jpg').substring(8):lanyrdResponse[i].image_75;
+
+                    var speaker = {
+                        'name':lanyrdResponse[i].name,
+                        'image':speaker_image,
+                        'twitter':'http://twitter.com/'+twitter,
+                        'break':(((counter+1)%4==0)?'clearfix':'')
+                    };
+                    speakerObject[counter] = speaker;
+                    counter++;
+                }
+            }
+            res.render('index',{
+                'title':app.locals.title,
+                'speakerObject' : speakerObject
+            });
+        });
+    }).on('error', function(e) {
+          console.log("Got error: ", e);
     });
 });
 
