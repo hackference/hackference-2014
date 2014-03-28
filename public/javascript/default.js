b00910cd1890581517a729deda766436a775e887
Mike Elsmore <mike@elsmore.me>
2014-03-28 22:59:29 +0000

    Add new navigation

--- public/javascript/default.js
+++ public/javascript/default.js
@@ -1,3 +1,28 @@
-/* Parallax */
-var scene = document.getElementById('scene');
-var parallax = new Parallax(scene);
+/* Nav stuff */
+$(function() {
+    // Stick the #nav to the top of the window
+    var nav = $('header nav');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
+});
